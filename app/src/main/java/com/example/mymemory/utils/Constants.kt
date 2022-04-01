d3104abdacc8df4ffe5a95981271195d0fb4b442
dibin <dibin.nadar@jiva.ag>
2022-04-01 18:06:33 +0530

    Added default image resources

---
+++ app/src/main/java/com/example/mymemory/utils/Constants.kt
@@ -0,0 +1,18 @@
+package com.example.mymemory.utils
+
+import com.example.mymemory.R
+
+val DEFAULT_ICONS = listOf(
+    R.drawable.ic_boil_alive,
+    R.drawable.ic_cake,
+    R.drawable.ic_concoction,
+    R.drawable.ic_dance,
+    R.drawable.ic_drink,
+    R.drawable.ic_droid,
+    R.drawable.ic_face,
+    R.drawable.ic_flower,
+    R.drawable.ic_palette,
+    R.drawable.ic_paw,
+    R.drawable.ic_taxi,
+    R.drawable.ic_weird_egg
+)
