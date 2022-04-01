254fcdff959887c20a946edb25817f278197ae7c
dibin <dibin.nadar@jiva.ag>
2022-04-01 15:01:09 +0530

    Finished Segment 2

--- app/src/main/java/com/example/mymemory/MemoryBoardAdapter.kt
+++ app/src/main/java/com/example/mymemory/MemoryBoardAdapter.kt
@@ -1,9 +1,11 @@
 package com.example.mymemory
 
 import android.content.Context
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageButton
 import androidx.cardview.widget.CardView
 import androidx.recyclerview.widget.RecyclerView
 import kotlin.math.min
@@ -16,9 +18,16 @@ class MemoryBoardAdapter(private val context: Context, private val numMemoryPiec
 /**                                Using custom viewHolder*/
         RecyclerView.Adapter<MemoryBoardAdapter.ViewHolder>() {
 
+        /** COs are singletons where constants are defined. Members are accessible directly through the CC
+         *  Like Static variables in Java */
+        companion object {
+            private const val MARGIN_SIZE = 10
+            private const val TAG = "MemoryBoardAdapter"
+        }
+
         override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-            val cardWidth = parent.width / 2
-            val cardHeight = parent.height / 4
+            val cardWidth = parent.width / 2 - (2 * MARGIN_SIZE)
+            val cardHeight = parent.height / 4 - (2 * MARGIN_SIZE)
             val cardSideLength = min(cardHeight, cardWidth)
             val view = LayoutInflater.from(context).inflate(
                 R.layout.memory_card,
@@ -26,9 +35,11 @@ class MemoryBoardAdapter(private val context: Context, private val numMemoryPiec
                 false
             ) // inflate returns the actual view which was created
 
-            val layoutParams = view.findViewById<CardView>(R.id.cardView).layoutParams
+            val layoutParams =
+                view.findViewById<CardView>(R.id.cardView).layoutParams as ViewGroup.MarginLayoutParams
             layoutParams.width = cardSideLength
             layoutParams.height = cardSideLength
+            layoutParams.setMargins(MARGIN_SIZE, MARGIN_SIZE, MARGIN_SIZE, MARGIN_SIZE)
 
             return ViewHolder(view)
         }
@@ -40,8 +51,12 @@ class MemoryBoardAdapter(private val context: Context, private val numMemoryPiec
         }
 
         inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+            private val imageButton = itemView.findViewById<ImageButton>(R.id.imageButton)
+
             fun bind(position: Int) {
-//                TODO("Not yet implemented")
+                imageButton.setOnClickListener {
+                    Log.i(TAG, "Clicked on position: $position")
+                }
             }
         }
     }
