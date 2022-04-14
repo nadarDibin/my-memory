cad012a79b5532eeaeca8fa9cb71824a34b0c326
dibin <dibin.nadar@jiva.ag>
2022-04-14 17:16:16 +0530

    Added a Recycler View for the Custom Screen

---
+++ app/src/main/java/com/example/mymemory/ImagePickerAdapter.kt
@@ -0,0 +1,53 @@
+package com.example.mymemory
+
+import android.content.Context
+import android.net.Uri
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import androidx.recyclerview.widget.RecyclerView
+import com.example.mymemory.models.GameBoard
+import kotlin.math.min
+
+class ImagePickerAdapter(
+    private val context: Context,
+    private val imageUris: List<Uri>,
+    private val board: GameBoard
+) : RecyclerView.Adapter<ImagePickerAdapter.ViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val view = LayoutInflater.from(context).inflate(R.layout.card_image, parent, false)
+        val cardWidth = parent.width / board.getWidth()
+        val cardHeight = parent.height / board.getHeight()
+        val cardSideLength = min(cardHeight, cardWidth)
+        val layoutParams = view.findViewById<ImageView>(R.id.ivCustomImage).layoutParams
+        layoutParams.width = cardSideLength
+        layoutParams.height = cardSideLength
+
+        return ViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        if (position < imageUris.size) {
+            holder.bind(imageUris[position])
+        } else {
+            holder.bind()
+        }
+    }
+
+    override fun getItemCount(): Int = board.getNumPairs()
+
+    inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        private val ivCustomImage = itemView.findViewById<ImageView>(R.id.ivCustomImage)
+        fun bind(uri: Uri) {
+            ivCustomImage.setImageURI(uri)
+            ivCustomImage.setOnClickListener(null)
+        }
+
+        fun bind() {
+            ivCustomImage.setOnClickListener {
+            }
+        }
+    }
+}
--- app/src/main/java/com/example/mymemory/utils/CreateActivity.kt
+++ app/src/main/java/com/example/mymemory/utils/CreateActivity.kt
@@ -1,25 +1,44 @@
 package com.example.mymemory.utils
 
+import android.net.Uri
 import android.os.Bundle
 import android.view.MenuItem
+import android.widget.Button
+import android.widget.EditText
 import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.mymemory.ImagePickerAdapter
 import com.example.mymemory.R
 import com.example.mymemory.models.GameBoard
 
 class CreateActivity : AppCompatActivity() {
 
+    private lateinit var rvImagePicker: RecyclerView
+    private lateinit var etGameName: EditText
+    private lateinit var btnSave: Button
+
     private lateinit var board: GameBoard
     private var numImagesRequired = -1
+    private val chosenImageUris = mutableListOf<Uri>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_create)
+
+        rvImagePicker = findViewById(R.id.rvImagePicker)
+        etGameName = findViewById(R.id.etGameName)
+        btnSave = findViewById(R.id.btnSave)
 
         /** generates the Back button */
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
         board = intent.getSerializableExtra(CUSTOM_BOARD_SIZE) as GameBoard
         numImagesRequired = board.getNumPairs()
         supportActionBar?.title = "Images selected (0/$numImagesRequired)"
+
+        rvImagePicker.adapter = ImagePickerAdapter(this, chosenImageUris, board)
+        rvImagePicker.setHasFixedSize(true)
+        rvImagePicker.layoutManager = GridLayoutManager(this, board.getWidth())
     }
 
     /** Dictates the behaviour of the Back button */
